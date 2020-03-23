76132d8ead6711e8a219123cf9cf1d5a36af77fb
charleseduardome <charleseduardo.me@gmail.com>
2020-03-23 14:54:59 -0300

    React Navegation e Background

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SignIn from '~/pages/SignIn';
+import SignUp from '~/pages/SignUp';
+
+const Stack = createStackNavigator();
+
+export default function createRouter() {
+  return (
+    <Stack.Navigator headerMode="none">
+      <Stack.Screen
+        name="SignIn"
+        component={SignIn}
+        options={{ title: 'SignIn' }}
+      />
+      <Stack.Screen name="SignUp" component={SignUp} />
+    </Stack.Navigator>
+  );
+}
