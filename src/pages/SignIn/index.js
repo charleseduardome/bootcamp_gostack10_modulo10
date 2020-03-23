902470423c91560fa72b39172d842180f17962e0
charleseduardome <charleseduardo.me@gmail.com>
2020-03-23 16:14:57 -0300

    Components Input e Button

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-// import { Container } from './styles';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 import Background from '~/components/Background';
+
+// import { Container } from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>exemplo</Text>
+      <Text>SignIn</Text>
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+      <Button>Entrar</Button>
     </Background>
   );
 }
