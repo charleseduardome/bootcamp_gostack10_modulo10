1948c7a16fe54e1aa4e3dc72c84fa7fbfc16b2e0
charleseduardome <charleseduardo.me@gmail.com>
2020-03-25 11:54:44 -0300

    Funcionalidade de agendamento

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
+
+import PropTypes from 'prop-types';
 
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -7,11 +10,79 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
+
+import SelectProvider from '~/pages/New/SelectProvider';
+import SelectDateTime from '~/pages/New/SelectDateTime';
+import Confirm from '~/pages/New/Confirm';
+
 import Dashboard from '~/pages/Dashboard';
 import Profile from '~/pages/Profile';
 
 const Stack = createStackNavigator();
 const Tabs = createBottomTabNavigator();
+
+function NewStack({ navigation }) {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerTransparent: true,
+        headerTintColor: '#FFF',
+        headerLeftContainerStyle: {
+          marginLeft: 20,
+        },
+      }}
+    >
+      <Stack.Screen
+        name="SelectProvider"
+        component={SelectProvider}
+        options={{
+          title: 'Selecione o prestador',
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('Dashboard');
+              }}
+            >
+              <Icon name="chevron-left" size={20} color="#FFF" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="SelectDateTime"
+        component={SelectDateTime}
+        options={{
+          title: 'Selecione o horário',
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('SelectProvider');
+              }}
+            >
+              <Icon name="chevron-left" size={20} color="#FFF" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="Confirm"
+        component={Confirm}
+        options={{
+          title: 'Confirmar agendamento',
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('SelectDateTime');
+              }}
+            >
+              <Icon name="chevron-left" size={20} color="#FFF" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
 
 export default function createRouter(isSigned = false) {
   return !isSigned ? (
@@ -42,6 +113,19 @@ export default function createRouter(isSigned = false) {
       />
 
       <Tabs.Screen
+        name="New"
+        component={NewStack}
+        options={{
+          unmountOnBlur: true,
+          tabBarVisible: false,
+          tabBarLabel: 'Agendar',
+          tabBarIcon: ({ color }) => (
+            <Icon name="add-circle-outline" size={20} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
         name="Profile"
         component={Profile}
         options={{
@@ -54,3 +138,10 @@ export default function createRouter(isSigned = false) {
     </Tabs.Navigator>
   );
 }
+
+NewStack.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+    goBack: PropTypes.func,
+  }).isRequired,
+};
